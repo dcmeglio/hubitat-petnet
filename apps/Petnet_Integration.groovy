1c394e430412867329a8c25054bf0acb698ecf7f
dcmeglio <dmeglio@gmail.com>
2019-08-22 19:31:26 -0400

    Initial commit

---
+++ apps/Petnet_Integration.groovy
@@ -0,0 +1,163 @@
+/**
+ *  Petnet Integration
+ *
+ *  Copyright 2019 Dominick Meglio
+ *
+ */
+
+import groovy.transform.Field
+
+definition(
+    name: "Petnet Integration",
+    namespace: "dcm.petnet",
+    author: "Dominick Meglio",
+    description: "Connects to BOND Home hub",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+	page(name: "prefAccount", title: "Petnet")
+}
+
+def prefAccount() {
+	return dynamicPage(name: "prefAccount", title: "Connect to Petnet", nextPage:"prefListDevices", uninstall:true, install: true) {
+		section("Account Information"){
+			input("pnUsername", "text", title: "Petnet Username", description: "Petnet Username")
+			input("pnPassword", "password", title: "Petnet Password", description: "Petnet Password")
+            input("debugOutput", "bool", title: "Enable debug logging?", defaultValue: true, displayDuringSetup: false, required: false)
+		}
+	}
+}
+
+@Field appToken = "3Ei86ExIh6USKcuMJMrPgg=="
+@Field apiUrl = "https://m-api570.petnet.io"
+@Field appVersion = "5.7.4"
+
+
+def installed() {
+	logDebug "Installed with settings: ${settings}"
+
+	initialize()
+}
+
+def updated() {
+	logDebug "Updated with settings: ${settings}"
+    unschedule()
+	unsubscribe()
+	initialize()
+}
+
+def uninstalled() {
+	logDebug "Uninstalled app"
+
+	for (device in getChildDevices())
+	{
+		deleteChildDevice(device.deviceNetworkId)
+	}	
+}
+
+def initialize() {
+	logDebug "initializing"
+    
+    authorize()
+    
+    getDevices()
+}
+
+def authorize() 
+{
+    def result = sendApiRequest("/tokens", "GET", null)
+    logDebug "tokens result: ${result.data}"
+    if (result.data.status == "ok")
+    {
+        state.firebaseUrl = result.data.tokens.firebase.url
+        state.firebaseToken = result.data.tokens.firebase.token
+        logDebug "got firebase info"
+        result = sendApiRequest("/session", "POST", [
+            user: [
+                email: pnUsername,
+                password: pnPassword
+            ]
+        ])
+        logDebug "session result: ${result.data}"
+        if (result.data.success == true)
+        {
+            state.apiToken = result.data.api_token
+            state.apiCookie = result?.headers?.'Set-Cookie'?.split(';')?.getAt(0)
+        }
+    }
+}
+
+def getDevices()
+{
+    def result = sendApiRequest("/user/devices", "GET", null)
+    
+    logDebug "got devices ${result.data}"
+    if (result.data.status == "ok")
+    {
+        for (def i = 0; i < result.data.devices.size(); i++)
+        {
+            def deviceId = result.data.devices[i]["device"]["id"]
+            def petFeeder = getChildDevice("petnet:" + deviceId)
+            if (!petFeeder)
+            {    
+                petFeeder = addChildDevice("petnet", "Petnet Feeder", "petnet:" + deviceId, 1234, ["name": "Petnet Feeder ${i+1}", isComponent: false])
+            }
+            petFeeder.updateDataValue("status_url", result.data.devices[i]["device"]["status_url"])
+        }
+    }
+}
+
+def sendApiRequest(path, method, body)
+{
+    def params = [
+		uri: "https://${apiUrl}",
+        path: "${path}",
+		contentType: "application/json",
+        requestContentType: "application/json",
+        headers: [
+            "Accept": "application/json;version=${appVersion}"
+        ],
+		query: [
+                app_token: appToken
+            ]
+	]
+    if (state.apiToken != null)
+        params.query["api_token"] = state.apiToken
+    
+    if (state.apiCookie != null)
+        params.headers["Cookie"] = state.apiCookie
+    
+    if (body != null)
+        params.body = body
+    
+    def result = null
+    if (method == "GET") {
+        httpGet(params) { resp ->
+            result = resp
+        }
+    }
+    else if (method == "POST") {
+        httpPost(params) { resp ->
+            result = resp
+        }
+    }
+    return result
+}
+
+def getFirebaseToken() {
+    return state.firebaseToken;
+}
+
+def getFirebaseUrl() {
+    return state.firebaseUrl;
+}
+
+def logDebug(msg) {
+    if (settings?.debugOutput) {
+		log.debug msg
+	}
+}
---
+++ drivers/Petnet_Feeder.groovy
@@ -0,0 +1,147 @@
+/**
+ *  Petnet Feeder
+ *
+ *  Copyright 2019 Dominick Meglio
+ *
+ */
+metadata {
+    definition (name: "Petnet Feeder", namespace: "petnet", author: "Dominick Meglio") {
+		capability "Momentary"
+		capability "PushableButton"
+        capability "Initialize" 
+		
+		attribute "hopperLevel", "string"
+    }
+}
+
+preferences {
+    input "feedAmount", "enum", title: "Feeding Amount (in Cups)" , options: ["0.0625":"1/16", "0.125":"1/8", "0.1875":"3/16", "0.25":"1/4", "0.3333": "1/3", "0.375":"3/8", "0.4375":"7/16", "0.5":"1/2", "0.5625":"9/16", "0.6666":"2/3", "0.6875":"11/16", "0.75":"3/4", "0.8125":"13/16", "0.875":"7/8", "0.9375":"15/16", "1":"1" ]
+    input "debugOutput", "bool", title: "Enable debug logging?", defaultValue: true, displayDuringSetup: false, required: false
+}
+def installed() {
+	sendEvent(name: "numberOfButtons", value: "1")
+    runIn(5, initialize)
+}
+
+def initialize()
+{
+    log.debug getDataValue("status_url")+".json?auth="+parent.getFirebaseToken()
+    eventStreamConnect(getDataValue("status_url")+".json?auth="+parent.getFirebaseToken(), "Bearer " + parent.getFirebaseToken())
+}
+
+def push() {
+	if (feed())
+        sendEvent(name: "pushed", value: 1,  isStateChange: true)
+}
+
+def parse(description) {
+     def json = parseJson(description)
+    
+    if (json.path == "/server/hopper/last_filled")
+    {
+        sendEvent(name: "hopperLevel", value: json.data.cups_remaining)
+    }
+    else if (json.path == "/server")
+    {
+        sendEvent(name: "hopperLevel", value: json.data["hopper/cups_remaining"])
+    }
+    else if (json.path == "/device/realtime/feeding")
+    {
+        if (data.progress == 1)
+            queryHopper()
+    }
+}
+
+def queryHopper()
+{
+    def statusPath = getDataValue("status_url").replace(parent.getFirebaseUrl(),"") + "/server/hopper.json"
+    def params = [
+		uri: parent.getFirebaseUrl(),
+        path: statusPath,
+		contentType: "application/json",
+        requestContentType: "application/json",
+        query:
+        [
+            auth: parent.getFirebaseToken()
+        ]
+    ]
+    def result = false
+    try
+    {
+        httpGet(params) { resp -> 
+            result = true
+            sendEvent(name: "hopperLevel", value: resp.data.cups_remaining)
+        }
+    }
+    catch (e)
+    {
+        log.error e
+        result = false
+    }
+    return result
+}
+
+def feed()
+{
+    def feedPath = getDataValue("status_url").replace(parent.getFirebaseUrl(),"") + "/mobile/commands/feed.json"
+    def params = [
+		uri: parent.getFirebaseUrl(),
+        path: feedPath,
+		contentType: "application/json",
+        requestContentType: "application/json",
+        query:
+        [
+            auth: parent.getFirebaseToken()
+        ],
+        body: '{"portion": ' + feedAmount + "}"
+    ]
+    def result = false
+    try
+    {
+        httpPut(params) { resp -> 
+            result = true
+        }
+    }
+    catch (e)
+    {
+        log.error e
+        result = false
+    }
+    return result
+}
+
+def eventStreamStatus(String msg) {
+	if(!msg.contains("ALIVE:")) 
+    { 
+        logDebug "Status: ${msg}" 
+    }
+	if (msg.contains("STOP:")) 
+    {
+        stopEventStream()
+        runIn(10, reconnect)
+	} 
+    else if (msg.contains("ALIVE:")) 
+    {
+		state.streamRunning = true
+	} 
+    else if (msg.contains("ERROR:")) 
+    {
+        stopEventStream()
+        runIn(10, reconnect)
+    }
+}
+
+def reconnect() {
+    initialize()
+}
+
+def stopEventStream() {
+    state.streamRunning = false
+    eventStreamClose()
+}
+
+def logDebug(msg) {
+    if (settings?.debugOutput) {
+		log.debug msg
+	}
+}
